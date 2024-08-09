23f67ccb2ee5f04666db03d5462087d7dcd593b0
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-09 12:31:40 +0200

    Feat: add context providers for authentication, general errors, and modals

---
+++ src/contexts/ModalContexts.jsx
@@ -0,0 +1,54 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useMemo,
+  useCallback,
+} from 'react';
+import PropTypes from 'prop-types';
+
+export const ModalContext = createContext();
+
+// ModalContext is created to manage the state of login and signup modals across the application.
+// This context provides functions to open and close the modals and tracks whether each modal is currently open or closed.
+export function ModalProvider({ children }) {
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+  const [isSignupModalOpen, setSignupModalOpen] = useState(false);
+
+  const openLoginModal = useCallback(() => setLoginModalOpen(true), []);
+  const closeLoginModal = useCallback(() => setLoginModalOpen(false), []);
+  const openSignupModal = useCallback(() => setSignupModalOpen(true), []);
+  const closeSignupModal = useCallback(() => setSignupModalOpen(false), []);
+
+  // Memoize the context value to avoid unnecessary re-renders
+  const contextValue = useMemo(
+    () => ({
+      isLoginModalOpen,
+      isSignupModalOpen,
+      openLoginModal,
+      closeLoginModal,
+      openSignupModal,
+      closeSignupModal,
+    }),
+    [
+      isLoginModalOpen,
+      isSignupModalOpen,
+      openLoginModal,
+      closeLoginModal,
+      openSignupModal,
+      closeSignupModal,
+    ],
+  );
+
+  return (
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+ModalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useModal = () => useContext(ModalContext);
---
+++ src/contexts/index.jsx
@@ -0,0 +1,15 @@
+import { AuthContext, AuthProvider } from './AuthContext';
+import { ModalContext, ModalProvider } from './ModalContexts';
+import {
+  GeneralErrorContext,
+  GeneralErrorProvider,
+} from './GeneralErrorContext';
+
+export {
+  AuthContext,
+  AuthProvider,
+  ModalContext,
+  ModalProvider,
+  GeneralErrorContext,
+  GeneralErrorProvider,
+};
