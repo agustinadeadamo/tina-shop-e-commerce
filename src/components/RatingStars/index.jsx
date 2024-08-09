f2f2d9b283f83cd4251e202068cbee2b703cdee2
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-09 12:16:19 +0200

    Feat: Created Rating Stars component

---
+++ src/components/RatingStars/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaStar, FaRegStar } from 'react-icons/fa';
+
+function RatingStars({ rate, size = '' }) {
+  const roundedRate = Math.round(rate);
+
+  return (
+    <div className="flex items-center mt-2 mb-2">
+      {Array.from({ length: 5 }, (_, index) => {
+        const Icon = index < roundedRate ? FaStar : FaRegStar;
+        return <Icon key={index} className={`text-secondary ${size}`} />;
+      })}
+    </div>
+  );
+}
+
+RatingStars.propTypes = {
+  rate: PropTypes.number.isRequired,
+  size: PropTypes.string,
+};
+
+export default React.memo(RatingStars);
