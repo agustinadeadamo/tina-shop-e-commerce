0b6a94129e54e816ad6b51820427b380c48f41d0
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-09 11:42:02 +0200

    Feat: Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className="bg-primary text-white py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center space-x-4">
+          {/* Social Media Icons */}
+          <a href="#" aria-label="Facebook" className="text-white">
+            <FaFacebook className="w-6 h-6" />
+          </a>
+          <a href="#" aria-label="Twitter" className="text-white">
+            <FaTwitter className="w-6 h-6" />
+          </a>
+          <a href="#" aria-label="Instagram" className="text-white">
+            <FaInstagram className="w-6 h-6" />
+          </a>
+        </div>
+        <div className="text-center mt-4">
+          <p>&copy; 2024 Agustina De Adamo Portfolio. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
