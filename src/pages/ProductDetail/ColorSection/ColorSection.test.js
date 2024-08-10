14acd05cda887ce9d077e3bf89da1b2bd4bea919
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-10 12:54:59 +0200

    tests: ColorSection - Created corresponding tests

--- src/pages/ProductDetail/ColorSection/ColorSection.test.js
+++ src/pages/ProductDetail/ColorSection/ColorSection.test.js
@@ -6,19 +6,16 @@ import possibleColors from './colorsData';
 describe('ColorSection', () => {
   it('renders the correct number of color options', () => {
     render(<ColorSection />);
-    const colorOptions = screen.getAllByRole('button');
-    expect(colorOptions).toHaveLength(possibleColors.length);
+    possibleColors.forEach(color => {
+      expect(screen.getByTestId(`color-option-${color}`)).toBeInTheDocument();
+    });
   });
 
   it('selects the correct color when an option is clicked', () => {
     render(<ColorSection />);
-
-    const secondColorOption = screen.getAllByRole('button')[1];
-
-    // Simula un clic en la segunda opción de color
+    const secondColor = possibleColors[1];
+    const secondColorOption = screen.getByTestId(`color-option-${secondColor}`);
     fireEvent.click(secondColorOption);
-
-    // Verifica que la segunda opción ahora tiene la clase que indica que está seleccionada
     expect(secondColorOption).toHaveClass('border-2 border-gray-800');
   });
 });
--- src/pages/ProductDetail/ColorSection/index.jsx
+++ src/pages/ProductDetail/ColorSection/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import ColorOption from "./ColorOption";
-import possibleColors from "./colorsData";
+import React, { useState } from 'react';
+import ColorOption from './ColorOption';
+import possibleColors from './colorsData';
 
 function ColorSection() {
   const [selectedColor, setSelectedColor] = useState(possibleColors[0]);
 
-  const handleColorChange = (color) => {
+  const handleColorChange = color => {
     setSelectedColor(color);
   };
 
@@ -13,12 +13,13 @@ function ColorSection() {
     <div className="relative mb-8">
       <h4 className="font-semibold mb-2">Choose a color:</h4>
       <div className="flex space-x-2 mb-4">
-        {possibleColors.map((color) => (
+        {possibleColors.map(color => (
           <ColorOption
             key={color}
             color={color}
             isSelected={selectedColor === color}
             onSelect={() => handleColorChange(color)}
+            dataTestid={`color-option-${color}`} // Add data-testid for each color option
           />
         ))}
       </div>
