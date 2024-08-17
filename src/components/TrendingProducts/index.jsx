155ea68d65daad3aa55ee2b781e786ca5011c8b9
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-17 18:06:43 +0200

    styles(TrendingProducts): udpdates styles and animation for TrendingProducts component

--- src/components/TrendingProducts/index.jsx
+++ src/components/TrendingProducts/index.jsx
@@ -1,42 +1,30 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import ResponsiveContainer from '../ResponsiveMainContainer';
 import ProductCard from '../ProductCard';
 import { useProductCardActions } from '../../hooks';
-import trendingProductsAnimation from './animations';
 
 const TrendingProducts = ({ products }) => {
-  const containerRef = useRef(null);
   const { handleAddSingleItemToCart, handleViewMore } = useProductCardActions();
   const productsExist = !products?.length;
-
-  useEffect(() => {
-    if (containerRef.current) {
-      trendingProductsAnimation(containerRef);
-    }
-  }, [products]);
 
   if (productsExist) return null;
 
   return (
-    <ResponsiveContainer>
-      <div className="mt-20 mb-20">
-        <h2 className="text-3xl font-semibold mb-10">Trending Products</h2>
-        <div
-          className="grid grid-cols-1 md:grid-cols-4 gap-4"
-          ref={containerRef}
-        >
-          {products.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              handleAddToCart={() => handleAddSingleItemToCart(product)}
-              handleViewMore={() => handleViewMore(product.id)}
-            />
-          ))}
-        </div>
+    <div className="mt-20 mb-20 p-4">
+      <h2 className="text-xl font-semibold mb-10 uppercase text-center">
+        Trending Products
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={() => handleAddSingleItemToCart(product)}
+            handleViewMore={() => handleViewMore(product.id)}
+          />
+        ))}
       </div>
-    </ResponsiveContainer>
+    </div>
   );
 };
 
