2fe76f1908ec449feff1f72c0732e93abd2fcacf
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-18 12:54:34 +0200

    chore(Loader): updated loader styles and min height

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Loader = () => {
   return (
     <div
-      className="min-h-[80vh] w-full flex items-center justify-center z-50"
+      className="min-h-[40vh] w-full flex items-center justify-center z-50"
       role="alert"
       aria-busy="true"
       aria-live="assertive"
     >
-      <div className="w-[36px] h-[36px] border-4 border-solid border-gray-200 border-l-[#ceabce] rounded-full animate-spin" />
+      <div className="w-[36px] h-[36px] border-4 border-solid border-gray-200 border-l-tertiary rounded-full animate-spin" />
     </div>
   );
 };
