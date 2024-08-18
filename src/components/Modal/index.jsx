f2963eccc038544f35d7567554d6f84cb2019e19
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-18 16:52:22 +0200

    refactor: Modified animation for modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 import { FaTimes } from 'react-icons/fa';
+import { contentVariants, containerVariants } from './animationVariants';
 
 const Modal = ({ isOpen, onClose, children, width = 'md:max-w-screen-md' }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div
-      className="fixed inset-0 flex items-center justify-center z-50 bg-gray-600 bg-opacity-50"
-      role="dialog"
-      aria-modal="true"
-      aria-labelledby="modal-title"
-      aria-describedby="modal-description"
-    >
-      <div
-        className={`bg-white rounded shadow-lg relative w-full h-full md:w-auto md:h-auto ${width} md:mx-auto`}
-        tabIndex="-1"
-      >
-        <button
-          type="button"
-          aria-label="close modal"
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={containerVariants}
+          className="fixed inset-0 flex items-center justify-center z-50 bg-gray-600 bg-opacity-50"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
         >
-          <FaTimes />
-        </button>
-        <div className="p-4 overflow-y-auto h-full" id="modal-description">
-          {children}
-        </div>
-      </div>
-    </div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={contentVariants}
+            className={`bg-white rounded shadow-lg relative w-full h-full md:w-auto md:h-auto ${width} md:mx-auto`}
+            tabIndex="-1"
+          >
+            <button
+              type="button"
+              aria-label="close modal"
+              onClick={onClose}
+              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+            >
+              <FaTimes />
+            </button>
+            <div className="p-4 overflow-y-auto h-full" id="modal-description">
+              {children}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
