83229cd31b020051a448f2a747979c04ad8afaf9
Agustina De Adamo <agustinadeadamo@MacBook-Pro-de-Agustina.local>
2024-08-20 15:41:28 +0200

    chore(FullScreeLoader): modified Fullscreenloader to start using Rotating text

--- src/components/FullScreenLoader/index.jsx
+++ src/components/FullScreenLoader/index.jsx
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
-import PropTypes from 'prop-types';
+import RotatingText from '../RotatingText';
 
-const FullScreenLoader = ({ onLoaded }) => {
-  const [count, setCount] = useState(23000); // Starting number
-  const target = 23100; // Target number of happy customers
-
-  useEffect(() => {
-    if (count < target) {
-      const interval = setInterval(() => {
-        setCount((prevCount) => prevCount + 1);
-      }, 10); // Speed of counting up
-
-      return () => clearInterval(interval);
-    }
-    // Notifies when counting is done
-    onLoaded();
-  }, [count, target, onLoaded]);
-
+const phrases = [
+  'the best deals for you...',
+  'your next great purchase...',
+  'exclusive surprises just for you...',
+  "amazing products you'll love...",
+  'your perfect shopping experience...',
+  'the best picks just for you...',
+  "trends you'll absolutely love...",
+  'something special, just for you...',
+  'your cart with incredible deals...',
+  'a unique shopping experience...',
+];
+const FullScreenLoader = () => {
   return (
     <motion.div
       className="fixed inset-0 bg-white flex items-center justify-center z-50"
@@ -25,15 +22,11 @@ const FullScreenLoader = ({ onLoaded }) => {
       transition={{ duration: 1, ease: 'easeInOut' }}
     >
       <div className="text-black text-3xl text-center text-montserrat">
-        <p> Unpacking {count}</p>
-        <p className="font-archivo">fresh looks.</p>
+        <p className="font-montserrat uppercase text-4xl mb-2">Loading</p>
+        <RotatingText fontSize="5xl" phrases={phrases} />
       </div>
     </motion.div>
   );
-};
-
-FullScreenLoader.propTypes = {
-  onLoaded: PropTypes.func.isRequired,
 };
 
 export default FullScreenLoader;
